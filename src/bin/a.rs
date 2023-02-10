98d4249470e9e6966e315d10b433b74f6e9aabdb
ntk_ta01 <43851479+ntk-ta01@users.noreply.github.com>
2023-02-10 12:46:48 +0900

    スコア計算関数を実装

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,13 +1,23 @@
 use itertools::Itertools;
 // use rand::prelude::*;
 
-// const DIJ: [(usize, usize); 4] = [(0, !0), (!0, 0), (0, 1), (1, 0)];
-// const DIR: [char; 4] = ['L', 'U', 'R', 'D'];
+const DIJ: [(usize, usize); 4] = [(0, !0), (!0, 0), (0, 1), (1, 0)];
+const DIR: [char; 4] = ['L', 'U', 'R', 'D'];
 
 fn main() {
     let input = read_input();
     let out: Output = Output::new(&input);
     write_output(&out);
+    eprintln!("score:{}", compute_score(&mut input.maps.clone(), &out));
+}
+
+#[derive(PartialEq, PartialOrd, Clone, Copy)]
+enum Square {
+    Player,
+    Coin,
+    Trap,
+    Wall,
+    Empty,
 }
 
 #[allow(dead_code)]
@@ -17,7 +27,7 @@ struct Input {
     h: usize,
     w: usize,
     t: usize,
-    maps: Vec<Vec<Vec<char>>>,
+    maps: Vec<Vec<Vec<Square>>>,
 }
 
 fn read_input() -> Input {
@@ -30,6 +40,24 @@ fn read_input() -> Input {
         t: usize,
         maps: [[Chars; h];n],
     }
+    let maps = maps
+        .into_iter()
+        .map(|map| {
+            map.into_iter()
+                .map(|row| {
+                    row.into_iter()
+                        .map(|ch| match ch {
+                            '@' => Square::Player,
+                            'o' => Square::Coin,
+                            'x' => Square::Trap,
+                            '#' => Square::Wall,
+                            _ => unreachable!("マップの入力が不正です"),
+                        })
+                        .collect()
+                })
+                .collect()
+        })
+        .collect();
     Input {
         n,
         k,
@@ -51,6 +79,42 @@ impl Output {
         let commands = (0..input.t).map(|_| 'L').collect();
         Output { maps, commands }
     }
+}
+
+fn compute_score(maps: &mut [Vec<Vec<Square>>], out: &Output) -> i64 {
+    let mut score = 0;
+    for &k in out.maps.iter() {
+        let map = &mut maps[k];
+        let (mut player_r, mut player_c) = find_player_position(map);
+        for &ch in out.commands.iter() {
+            if let Some(d) = DIR.iter().position(|dir_c| *dir_c == ch) {
+                let next_r = player_r + DIJ[d].0;
+                let next_c = player_c + DIJ[d].1;
+                match map[next_r][next_c] {
+                    Square::Wall => continue,
+                    Square::Trap => break,
+                    Square::Coin => score += 1,
+                    Square::Empty => {}
+                    Square::Player => unreachable!("プレイヤーが複数います"),
+                }
+                map[next_r][next_c] = Square::Empty;
+                player_r = next_r;
+                player_c = next_c;
+            }
+        }
+    }
+    score
+}
+
+fn find_player_position(map: &[Vec<Square>]) -> (usize, usize) {
+    for (r, row) in map.iter().enumerate() {
+        for (c, square) in row.iter().enumerate() {
+            if *square == Square::Player {
+                return (r, c);
+            }
+        }
+    }
+    unreachable!("map上にplayerが見つかりませんでした")
 }
 
 fn write_output(out: &Output) {
